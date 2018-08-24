c6538f863618509415557c52067f648d02bd2ad7
Koichi Miyamoto <m5813k@gmail.com>
2018-08-24 15:05:52 +0900

    canvas用意した

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <my-canvas/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import MyCanvas from "./components/Canvas";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    MyCanvas
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
---
+++ src/components/Canvas.vue
@@ -0,0 +1,20 @@
+<template>
+  <canvas class="canvas" ></canvas>
+</template>
+<script>
+import paper from "paper";
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+.canvas {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
