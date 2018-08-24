959adff1ddfa05c53072c40c4e2aab9a2275f960
Koichi Miyamoto <m5813k@gmail.com>
2018-08-24 16:05:10 +0900

    とりあえずcanvas表示

--- src/components/Canvas.vue
+++ src/components/Canvas.vue
@@ -1,11 +1,40 @@
 <template>
-  <canvas class="canvas" ></canvas>
+  <canvas class="canvas" ref="canvas"></canvas>
 </template>
 <script>
 import paper from "paper";
+import {
+  WebGLRenderer,
+  Scene,
+  PerspectiveCamera,
+  OrthographicCamera,
+  AxesHelper
+} from "three";
 export default {
+  mounted() {
+    this.init();
+    this.animate();
+  },
+  methods: {
+    init() {
+      this.renderer = new WebGLRenderer({ canvas: this.$refs.canvas });
+      console.log(this.renderer);
+      const axis = new AxesHelper(100);
+      this.scene.add(axis);
+
+      this.camera.position.set(0, 0, 100);
+    },
+    animate() {
+      // requestAnimationFrame( animate );
+      this.renderer.render(this.scene, this.camera);
+    }
+  },
   data() {
-    return {};
+    return {
+      renderer: null,
+      scene: new Scene(),
+      camera: new OrthographicCamera(-100, 100, 100, -100)
+    };
   }
 };
 </script>
