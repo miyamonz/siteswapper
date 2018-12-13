b6d1624cacebeb07f4ac83a2e7dde7c58301ffd6
Koichi Miyamoto <m5813k@gmail.com>
2018-12-13 22:12:57 +0900

    gh-pagesやめてnetlifyにした

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, "../dist"),
     assetsSubDirectory: "static",
-    assetsPublicPath: "/siteswapper/",
+    assetsPublicPath: "/",
 
     /**
      * Source Maps
